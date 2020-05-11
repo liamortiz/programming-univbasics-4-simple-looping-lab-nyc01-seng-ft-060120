ea15a92bad8db4ea95cc8fda01b8dea572d2bdc6
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 01:10:14 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, counter)
+  counter.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  array.each{|e| puts e}
+end
+
+def return_string_array(array)
+  array.map{|e| e.to_s}
+end
